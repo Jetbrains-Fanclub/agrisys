bbee703d8dee65da21ab9b0c734edd1a810d5039
Pviksy <birkegaardm@gmail.com>
2023-11-20 10:50:54 +0100

    Added IdentityContext class

---
+++ Models/IdentityContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Agrisys.Models {
+    public class IdentityContext: IdentityDbContext<IdentityUser> {
+        public IdentityContext(DbContextOptions<IdentityContext> options)
+            : base(options) { }
+    }
+}
