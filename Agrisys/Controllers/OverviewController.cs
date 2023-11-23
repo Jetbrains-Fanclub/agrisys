ac817953268157055da63aaa69de7b9c563afb8e
Christiansgh <workmailchristian@gmail.com>
2023-11-23 03:55:55 +0100

    Added navbar and some basic UI elements.

---
+++ Agrisys/Controllers/OverviewController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Agrisys.Controllers;
+
+public class OverviewController : Controller {
+    public ViewResult Index() {
+        return View();
+    }
+}
+
+
