1b9938ba70d8d1be330e8c20df69adfd548fa5d8
Christiansgh <workmailchristian@gmail.com>
2023-11-29 02:17:12 +0100

    Added base layout to reduce HTML boilerplate. Hardcoded active color in the topbar.

--- Agrisys/Controllers/HomeController.cs
+++ Agrisys/Controllers/HomeController.cs
@@ -1,28 +1,9 @@
-﻿using System.Diagnostics;
-
-using Agrisys.Models;
-
 using Microsoft.AspNetCore.Mvc;
 
 namespace Agrisys.Controllers;
 
 public class HomeController : Controller {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger) {
-        _logger = logger;
-    }
-
-    public IActionResult Index() {
+    public ViewResult Index() {
         return View();
-    }
-
-    public IActionResult Privacy() {
-        return View();
-    }
-
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error() {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
 }
