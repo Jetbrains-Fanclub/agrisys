acca52914a44450ddc5caece535eff39741484f5
Pviksy <birkegaardm@gmail.com>
2023-12-04 15:46:51 +0100

    Implemented admin view to navigate to crud pages via topbar

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -1,12 +1,10 @@
-//Stub to test button highlighting & possible implementation
-
 using Microsoft.AspNetCore.Mvc;
 
 namespace Agrisys.Controllers;
 
 public class AdminController : Controller {
-    public ViewResult Index() {
-        return View("Unimplemented");
+    public IActionResult Index() {
+        return View();
     }
 }
 
